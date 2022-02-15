4f54aaef767476b3514863428a88d97f07dc8619
Soul <top.secret.work@yandex.ru>
2022-02-15 14:46:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("100101", "101001"));     // a: 37, b: 41
+        System.out.println(binOps.mult("100101", "101001"));    // a: 37, b: 41
+
+    }
+}
